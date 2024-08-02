335b7059d3206e586a3da91a34e403796753a1bc
wesley990 <wesley990@gmail.com>
2024-08-02 22:06:48 +1200

    rewrite the theme

--- lib/home/theme.dart
+++ lib/home/theme.dart
@@ -40,113 +40,105 @@ class AppTheme {
   // Component-specific theme methods
 
   /// Defines the appearance of [AppBar] widgets.
-  static AppBarTheme _appBarTheme(ColorScheme colorScheme) {
-    return AppBarTheme(
-      backgroundColor: colorScheme.secondary,
-      foregroundColor: colorScheme.onSecondary,
-      elevation: 0,
-    );
-  }
+  static AppBarTheme _appBarTheme(ColorScheme colorScheme) => AppBarTheme(
+        backgroundColor: colorScheme.secondary,
+        foregroundColor: colorScheme.onSecondary,
+        elevation: 0,
+      );
 
   /// Defines the appearance of input fields.
-  static InputDecorationTheme _inputDecorationTheme(ColorScheme colorScheme) {
-    return InputDecorationTheme(
-      border: OutlineInputBorder(
+  static InputDecorationTheme _inputDecorationTheme(ColorScheme colorScheme) =>
+      InputDecorationTheme(
+        border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(8),
-          borderSide: BorderSide(color: colorScheme.onPrimary)),
-      focusedBorder: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(8),
-        borderSide: BorderSide(color: colorScheme.onPrimary),
-      ),
-      fillColor: colorScheme.surface,
-    );
-  }
+          borderSide: BorderSide(color: colorScheme.onPrimary),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: BorderSide(color: colorScheme.onPrimary),
+        ),
+        fillColor: colorScheme.surface,
+      );
 
   /// Defines the appearance of [Card] widgets.
-  static CardTheme _cardTheme(ColorScheme colorScheme) {
-    return CardTheme(
-      color: colorScheme.surface,
-      elevation: 2,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-    );
-  }
+  static CardTheme _cardTheme(ColorScheme colorScheme) => CardTheme(
+        color: colorScheme.surface,
+        elevation: 2,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+      );
 
   /// Defines the text styles for the application.
-  static TextTheme _textTheme(ColorScheme colorScheme) {
-    return TextTheme(
-      titleLarge: TextStyle(
-        fontSize: 40,
-        fontWeight: FontWeight.bold,
-        color: colorScheme.onPrimary,
-      ),
-      titleMedium: TextStyle(
-        fontSize: 24,
-        fontWeight: FontWeight.bold,
-        color: colorScheme.onPrimary,
-      ),
-      bodyMedium: TextStyle(
-        fontSize: 20,
-        color: colorScheme.onPrimary,
-      ),
-    );
-  }
+  static TextTheme _textTheme(ColorScheme colorScheme) => TextTheme(
+        titleLarge: TextStyle(
+          fontSize: 40,
+          fontWeight: FontWeight.bold,
+          color: colorScheme.onPrimary,
+        ),
+        titleMedium: TextStyle(
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+          color: colorScheme.onPrimary,
+        ),
+        bodyMedium: TextStyle(
+          fontSize: 20,
+          color: colorScheme.onPrimary,
+        ),
+      );
 
   /// Defines the appearance of [TextButton] widgets.
-  static TextButtonThemeData _textButtonTheme(ColorScheme colorScheme) {
-    return TextButtonThemeData(
-      style: TextButton.styleFrom(
-        foregroundColor: colorScheme.onPrimaryFixed,
-        backgroundColor: colorScheme.primaryFixed,
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-      ),
-    );
-  }
+  static TextButtonThemeData _textButtonTheme(ColorScheme colorScheme) =>
+      TextButtonThemeData(
+        style: TextButton.styleFrom(
+          foregroundColor: colorScheme.onPrimaryFixed,
+          backgroundColor: colorScheme.primaryFixed,
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+        ),
+      );
 
   /// Defines the appearance of [ElevatedButton] widgets.
-  static ElevatedButtonThemeData _elevatedButtonTheme(ColorScheme colorScheme) {
-    return ElevatedButtonThemeData(
-      style: ElevatedButton.styleFrom(
-        foregroundColor: colorScheme.onPrimaryFixed,
-        backgroundColor: colorScheme.primaryFixed,
-        elevation: 10,
-        enableFeedback: true,
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-      ),
-    );
-  }
+  static ElevatedButtonThemeData _elevatedButtonTheme(
+          ColorScheme colorScheme) =>
+      ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          foregroundColor: colorScheme.onPrimaryFixed,
+          backgroundColor: colorScheme.primaryFixed,
+          elevation: 10,
+          enableFeedback: true,
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+        ),
+      );
 
   /// Defines the appearance of [OutlinedButton] widgets.
-  static OutlinedButtonThemeData _outlinedButtonTheme(ColorScheme colorScheme) {
-    return OutlinedButtonThemeData(
-      style: OutlinedButton.styleFrom(
-        foregroundColor: colorScheme.primaryFixed,
-        side: BorderSide(color: colorScheme.primaryFixed),
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-      ),
-    );
-  }
+  static OutlinedButtonThemeData _outlinedButtonTheme(
+          ColorScheme colorScheme) =>
+      OutlinedButtonThemeData(
+        style: OutlinedButton.styleFrom(
+          foregroundColor: colorScheme.primaryFixed,
+          side: BorderSide(color: colorScheme.primaryFixed),
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+        ),
+      );
 
   /// Defines the custom container theme.
-  static ContainerThemeData _containerTheme(ColorScheme colorScheme) {
-    return ContainerThemeData(
-      color: colorScheme.surface,
-      padding: const EdgeInsets.all(16),
-      margin: const EdgeInsets.symmetric(vertical: 8),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(8),
-        boxShadow: [
-          BoxShadow(
-            color: colorScheme.shadow.withOpacity(0.1),
-            blurRadius: 4,
-            offset: const Offset(0, 2),
-          ),
-        ],
-      ),
-    );
-  }
+  static ContainerThemeData _containerTheme(ColorScheme colorScheme) =>
+      ContainerThemeData(
+        color: colorScheme.surface,
+        padding: const EdgeInsets.all(16),
+        margin: const EdgeInsets.symmetric(vertical: 8),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          boxShadow: [
+            BoxShadow(
+              color: colorScheme.shadow.withOpacity(0.1),
+              blurRadius: 4,
+              offset: const Offset(0, 2),
+            ),
+          ],
+        ),
+      );
 
   // Utility methods for accessing theme properties
 
@@ -184,7 +176,7 @@ class ContainerThemeData extends ThemeExtension<ContainerThemeData> {
   final EdgeInsetsGeometry? margin;
   final BoxDecoration? decoration;
 
-  ContainerThemeData({
+  const ContainerThemeData({
     this.color,
     this.padding,
     this.margin,
@@ -197,14 +189,13 @@ class ContainerThemeData extends ThemeExtension<ContainerThemeData> {
     EdgeInsetsGeometry? padding,
     EdgeInsetsGeometry? margin,
     BoxDecoration? decoration,
-  }) {
-    return ContainerThemeData(
-      color: color ?? this.color,
-      padding: padding ?? this.padding,
-      margin: margin ?? this.margin,
-      decoration: decoration ?? this.decoration,
-    );
-  }
+  }) =>
+      ContainerThemeData(
+        color: color ?? this.color,
+        padding: padding ?? this.padding,
+        margin: margin ?? this.margin,
+        decoration: decoration ?? this.decoration,
+      );
 
   @override
   ContainerThemeData lerp(ThemeExtension<ContainerThemeData>? other, double t) {
@@ -257,7 +248,7 @@ extension BuildContextThemeExtension on BuildContext {
 /// ```dart
 /// Text(
 ///   'Hello, World!',
-///   style: AppTheme.titleLarge(context),
+///   style: context.titleLarge,
 /// )
 /// ```
 
@@ -265,9 +256,9 @@ extension BuildContextThemeExtension on BuildContext {
 ///
 /// ```dart
 /// Container(
-///   decoration: AppTheme.containerTheme(context).decoration,
-///   padding: AppTheme.containerTheme(context).padding,
-///   margin: AppTheme.containerTheme(context).margin,
+///   decoration: context.containerTheme.decoration,
+///   padding: context.containerTheme.padding,
+///   margin: context.containerTheme.margin,
 ///   child: YourWidget(),
 /// )
 /// ```
