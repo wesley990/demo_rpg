e380f441c8e4cae37c91e824fc533151293a2513
wesley990 <wesley990@gmail.com>
2024-08-06 13:50:27 +1200

    refactor: Update HomeScreen with best practices

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -2,74 +2,75 @@ import 'package:demo_rpg/screens/create/create.dart';
 import 'package:demo_rpg/screens/profile/profile.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:demo_rpg/services/character_store.dart';
-
-import 'package:demo_rpg/models/character.dart';
 import 'package:provider/provider.dart';
+
+import 'package:demo_rpg/services/character_store.dart';
+import 'package:demo_rpg/models/character.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: [
-        const SliverAppBar(
-          floating: true,
-          snap: true,
-          stretch: true,
-          pinned: false,
-          expandedHeight: 50,
-          flexibleSpace: FlexibleSpaceBar(
-            title: Text('Demo RPG'),
-            centerTitle: true,
-          ),
-        ),
-        Consumer<CharacterStore>(builder: (context, characterStore, child) {
-          return SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (context, index) {
-                final Character character = characterStore.characters[index];
-                return Card(
-                  child: ListTile(
-                    leading: SvgPicture.asset(
-                      'assets/images/vocations/${character.vocation.image}',
-                      height: 110,
-                    ),
-                    trailing: const Icon(Icons.arrow_forward_ios),
-                    title: Text(character.name),
-                    subtitle: Text(character.vocation.title),
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) =>
-                              ProfileScreen(character: character),
-                        ),
-                      );
-                    },
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Demo RPG'),
+        centerTitle: true,
+      ),
+      body: Consumer<CharacterStore>(
+        builder: (context, characterStore, child) {
+          return CustomScrollView(
+            slivers: [
+              SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (context, index) => _buildCharacterCard(
+                      context, characterStore.characters[index]),
+                  childCount: characterStore.characterCount,
+                ),
+              ),
+              SliverToBoxAdapter(
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: ElevatedButton(
+                    onPressed: () => _navigateToCreateScreen(context),
+                    child: const Text('Create'),
                   ),
-                );
-              },
-              childCount: characterStore.characterCount,
-            ),
+                ),
+              ),
+            ],
           );
-        }),
-        SliverToBoxAdapter(
-          child: Container(
-            padding: const EdgeInsets.all(20),
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const CreateScreen()),
-                );
-              },
-              child: const Text('Create'),
-            ),
-          ),
+        },
+      ),
+    );
+  }
+
+  Widget _buildCharacterCard(BuildContext context, Character character) {
+    return Card(
+      child: ListTile(
+        leading: SvgPicture.asset(
+          'assets/images/vocations/${character.vocation.image}',
+          height: 110,
         ),
-      ],
+        trailing: const Icon(Icons.arrow_forward_ios),
+        title: Text(character.name),
+        subtitle: Text(character.vocation.title),
+        onTap: () => _navigateToProfileScreen(context, character),
+      ),
+    );
+  }
+
+  void _navigateToCreateScreen(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const CreateScreen()),
+    );
+  }
+
+  void _navigateToProfileScreen(BuildContext context, Character character) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+          builder: (context) => ProfileScreen(character: character)),
     );
   }
 }
