ac40159da6411a9ea32bc326ed1816aa86bec759
wesley990 <wesley990@gmail.com>
2024-08-06 18:52:17 +1200

    refactor: Update FirestoreService to use FirebaseFirestore.instance
    The FirestoreService class in firestore_service.dart has been updated to use FirebaseFirestore.instance directly instead of casting it to FirebaseFirestore. This change simplifies the code and ensures consistency with the usage of FirebaseFirestore throughout the project.

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -1,10 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class FirestoreService {
-  static final FirebaseFirestore _firestore =
-      FirebaseFirestore.instance.collection('characters') as FirebaseFirestore;
-
-  // Singleton instance
   static final FirestoreService _instance = FirestoreService._internal();
 
   factory FirestoreService() {
@@ -13,30 +9,28 @@ class FirestoreService {
 
   FirestoreService._internal();
 
-  // Function to get a reference to a collection
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
   CollectionReference<Map<String, dynamic>> getCollection(
       String collectionName) {
     return _firestore.collection(collectionName);
   }
 
-  // Function to get a document reference
   DocumentReference<Map<String, dynamic>> getDocument(
       String collectionName, String documentId) {
     return _firestore.collection(collectionName).doc(documentId);
   }
 
-  // Function to add a new document to a collection
-  Future<void> addDocument(String collectionName, Map<String, dynamic> data) {
+  Future<DocumentReference<Map<String, dynamic>>> addDocument(
+      String collectionName, Map<String, dynamic> data) {
     return _firestore.collection(collectionName).add(data);
   }
 
-  // Function to update an existing document
   Future<void> updateDocument(
       String collectionName, String documentId, Map<String, dynamic> data) {
     return _firestore.collection(collectionName).doc(documentId).update(data);
   }
 
-  // Function to delete a document
   Future<void> deleteDocument(String collectionName, String documentId) {
     return _firestore.collection(collectionName).doc(documentId).delete();
   }
